665c67d30782c6eea5be77a5c95c1d21f444feb7
IVANZHEGALIN <111979803+IVANZHEGALIN@users.noreply.github.com>
2023-03-03 00:05:40 +0300

    Created main screen administration

--- app/src/main/java/com/example/cedarshop/screens/AdminSreen.kt
+++ app/src/main/java/com/example/cedarshop/screens/AdminSreen.kt
@@ -1,8 +1,88 @@
 package com.example.cedarshop.screens
 
+
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.cedarshop.ui.theme.CedarShopTheme
+import com.example.cedarshop.ui.theme.DarkGreen
 
 @Composable
 fun AdminScreen(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                ,title = {
+                Text(
+                    "Список работников",
+                    fontSize = 22.sp,
+                    textAlign = TextAlign.Center,
+
+                )
+            },
+                backgroundColor = DarkGreen,
+                contentColor = Color.White,
+                elevation = 12.dp,
+            )
+        },
+
+        floatingActionButton = {
+            FloatingActionButton(
+                content = {
+                    Icon(Icons.Filled.Clear, contentDescription = "Удалить")
+                },
+                onClick = { }
+            )
+        },
+        bottomBar = {
+            BottomAppBar {
+                IconButton(onClick = { }) { Icon(Icons.Filled.Menu, contentDescription = "Меню") }
+                Spacer(Modifier.weight(1f, true))
+                IconButton(onClick = { }) {
+                    Icon(
+                        Icons.Filled.Search,
+                        contentDescription = "Поиск"
+                    )
+                }
+            }
+
+
+        }
+
+    )
+    {
+        Text(text = "Что тот про задание", fontSize = 28.sp)
+    }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun PrevAdminScreen() {
+    CedarShopTheme() {
+        AdminScreen(navController = rememberNavController())
+    }
+}
+
+
+
+/*modifier = Modifier
+.fillMaxWidth(),
+
+contentPadding = PaddingValues(horizontal = 90.dp) */
--- app/src/main/java/com/example/cedarshop/screens/Personnel.kt
+++ app/src/main/java/com/example/cedarshop/screens/Personnel.kt
@@ -1,84 +1,21 @@
 package com.example.cedarshop.navigation
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-
-class Personnel(
-    val name: String,
-    val age: Int,
-    val number: Long,
-    val position: String
+class Jackets(
+    val seasonality: String,
+    val material: String ,
+    val gender: String ,
+    private var wear:Int = 100
 )
 {
-    fun work() {
-    }
+    fun heat() {}
+    fun protect() {}
+    private fun calculateWear(time:Int){
+        if (time>10){
+            wear = 90
+
 }
-
-
-
-open class ShopAssisten(
-    val id: Personnel,
-    val consult: String,
-) {
-    fun sell() {
-
-    }
 }
-
-class Ivan(
-    id: Personnel,
-    consult: String
-) : ShopAssisten
-    (
-    id = Personnel
-        (
-        "Ivan",
-        25,
-        89065537339,
-        "manager"
-    ),
-    "вам подсказать?",
-
-    ) {
-    fun sall() {
-
-    }
 }
-
-class Manager() {
-    init {
-        val managerIlya = Personnel(
-        "Ilya",
-        26,
-        89612546584,
-        "top manager")
-    }
-}
- val manager = Manager()
-
-
-val ivan = Manager()
-
-
-
-
-class Worker(
-    var age: Int,
-    private var moneylaundering : String,
-    var salary: Int
-)
-{
- fun cry() = "Все заебло"
-}
-
-
 
 
 
