d6e7dd5337a1dd656d3802b3afc6f1989867a839
IVANZHEGALIN <111979803+IVANZHEGALIN@users.noreply.github.com>
2023-04-28 00:01:55 +0300

    Make USER PAGE

--- app/src/main/java/com/example/cedarshop/presentation/pageUserAdmin/UserState.kt
+++ app/src/main/java/com/example/cedarshop/presentation/pageUserAdmin/UserState.kt
@@ -1,8 +1,8 @@
 package com.example.cedarshop.presentation.pageUserAdmin
 
 data class UserState(
-    val firstName: String = "",
-    val lastName: String = "",
+    val firstName: String = "Иван",
+    val lastName: String = "Жегалин",
     val position: String = "",
     val Number: String = ""
 )
